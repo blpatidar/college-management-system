def39251f09a3dcdb3041001c9edaa62fe5c51b2
jaibapna <jai.bapna@insigniac.com>
2018-06-07 12:07:18 +0530

    Now run angular and node on same port

--- collegeMangment/app.js
+++ collegeMangment/app.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+app.use(express.static('./AngularJS'));//angular and node on same port
 app.all("/User", function (req, res) {
     UserCtl.service(req, res);
 });
